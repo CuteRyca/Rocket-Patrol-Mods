7a027ab33accf7e39c11f69952f2c63a3ec92132
Your <handle@email.com>
2021-06-28 05:18:00 -0700

    Add background music to the Play scene

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -5,7 +5,7 @@ class Menu extends Phaser.Scene {
     
     preload(){
         //load audio
-        this.load.audio('sfx_select', './assets/blip_select12.wav');
+        this.load.audio('background_music', './assets/bensound-cute.mp3');
         this.load.audio('sfx_explosion', './assets/explosion38.wav');
         this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
     }
@@ -36,7 +36,7 @@ class Menu extends Phaser.Scene {
                 spaceshipSpeed: 3,
                 gameTimer : 60000
             }
-            this.sound.play('sfx_select');
+            this.sound.play('background_music');
             this.scene.start('playScene');
         }
         if(Phaser.Input.Keyboard.JustDown(keyRIGHT)){
@@ -44,7 +44,7 @@ class Menu extends Phaser.Scene {
                 spaceshipSpeed: 4,
                 gameTimer : 45000
             }
-            this.sound.play('sfx_select');
+            this.sound.play('background_music');
             this.scene.start('playScene');
         }
     }
