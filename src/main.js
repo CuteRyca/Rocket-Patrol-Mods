eeb28fef925d66ff8e4cec43b8c8664290c74449
Your <handle@email.com>
2021-06-29 04:50:33 -0700

    finished

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
+//Ryca Zhang, Rocket Patrol Mods, 6/29/2021. It takes me 6 hours.
+//Add your own (copyright-free) background music to the Play scene (5)
+//Create a new scrolling tile sprite for the background (5)
+//Create a new title screen (e.g., new artwork, typography, layout) (10)
+//Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (20)
+//Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (60)
+//(Theme: In this game, you are a farmer. One day you see there are many annoying birds flying around your house. Hold up your gun and shoot them! )
+//(I created eagles, bullets, crows, background and new explosion animation using Aseprite.)
+//sound effects credit: gunshot.wav:https://sc.chinaz.com/yinxiao/181022062272.htm
+//die.wav:https://sc.chinaz.com/yinxiao/170801507230.htm
+
+
+
+
 let config = {
     type: Phaser.CANVAS,
     width: 640,
